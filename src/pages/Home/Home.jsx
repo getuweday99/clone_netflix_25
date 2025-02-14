fa761709d9d84ee45d233919bef407a00210dd36
getuweday99 <getuweday99@gmail.com>
2025-02-14 07:52:08 +0300

    gg

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Banner from "../../components/Banner/Banner";
+import RowList from "../../components/Row/RowList/RowList";
+import Footer from "../../components/Footer/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <RowList />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
